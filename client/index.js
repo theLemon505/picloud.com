599c844bf396165f4f18596bce27721d89c5221e
theLemon505 <76224503+theLemon505@users.noreply.github.com>
2022-02-03 12:16:29 -0600

    Initial commit

---
+++ client/index.js
@@ -0,0 +1,19 @@
+function donate(){
+
+}
+
+function home(){
+    document.location.href = "/client/index.html"
+}
+
+function contact(){
+    document.location.href = "/client/pages/contact/contact.html"
+}
+
+function services(){
+
+}
+
+function signup(){
+
+}
